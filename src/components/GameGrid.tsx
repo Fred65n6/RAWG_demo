9955a06c5f5c11d01023903321dc5e6ada01c93e
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2023-09-06 12:37:14 +0200

    app

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
-import { Image } from "@chakra-ui/image";
 import useGames from "../hooks/useGames";
+import {SimpleGrid} from '@chakra-ui/react'
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error } = useGames();
@@ -7,12 +8,21 @@ const GameGrid = () => {
   return (
     <>
       {error && <p>{error}</p>}
-      {games.map((game) => (
-        <div key={game.id}>
-          <p>{game.name}</p>
-          <Image src={game.background_image} alt={game.name} />
-        </div>
+
+      <SimpleGrid
+      padding={"10px"}
+      spacing={10}
+      columns={{
+        base: 1,
+        md: 2,
+        lg: 3,
+        xl: 4,
+        }}
+      >
+        {games.map((game) => (
+        <GameCard key={game.id} game={game} />
       ))}
+      </SimpleGrid>
     </>
   );
 };
