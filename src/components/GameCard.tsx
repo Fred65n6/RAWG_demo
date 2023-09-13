9d39a207138afd647f80c195efe961cf0a223a06
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2023-09-13 09:03:03 +0200

    fixed gamecard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ game: Game;
 const GameCard = ({ game }: Props) => {
 return (
     <>
-    <Card>
+    <Card borderRadius={10} overflow={'hidden'}>
         <Image src={game.background_image} />
         <CardBody>
         <Heading>{game.name}</Heading>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const apiClient = axios.create ({
     baseURL: 'https://api.rawg.io/api/', 
-    params: { key: import.meta.env.VITE_API_KEY
+    params: { 
+        key: import.meta.env.VITE_API_KEY
     },
 
 });
